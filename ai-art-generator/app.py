29d22692cccdde99ed6c96159a2706dc43d35850
Kamal Debnath <76668054+kamaldebnath@users.noreply.github.com>
2023-01-22 01:16:14 +0530

    Update app.py

--- ai-art-generator/app.py
+++ ai-art-generator/app.py
@@ -2,7 +2,9 @@ import openai
 import os
 import streamlit as st
 import urllib.request as rq
-openai.api_key=st.secrets["api"]
+import config
+
+openai.api_key=config.api
 
 st.title("Image Generator Using AI")
 promt=st.text_input("Give a detailed description")
@@ -15,4 +17,3 @@ if promt:
   )
   image_url = response['data'][0]['url']
   st.image(image=image_url)
-
